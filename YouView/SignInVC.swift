d4e245dbf17e681b6644dc9469cdecbcf671fd86
Matt Tripodi <mtripodi1027@gmail.com>
2017-07-18 16:48:06 -0400

    Completed setting up Firebase intergration

---
+++ YouView/SignInVC.swift
@@ -0,0 +1,19 @@
+//
+//  SignInVC.swift
+//  YouView
+//
+//  Created by Matt Tripodi on 7/18/17.
+//  Copyright © 2017 Matthew Tripodi. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+
+}
+
