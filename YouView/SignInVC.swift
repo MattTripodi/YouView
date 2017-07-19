ed06f581416b6098d26e72664a466c9f74aca753
Matt Tripodi <mtripodi1027@gmail.com>
2017-07-19 00:38:16 -0400

    Added email authentication

--- YouView/SignInVC.swift
+++ YouView/SignInVC.swift
@@ -11,12 +11,19 @@ import FBSDKCoreKit
 import FBSDKLoginKit
 import Firebase
 
-class SignInVC: UIViewController {
+class SignInVC: UIViewController, UITextFieldDelegate {
 	
 	//IBOutlets
+	@IBOutlet weak var emailField: CustomTextField!
+	@IBOutlet weak var passwordField: CustomTextField!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		// To hide the keyboard when return is pressed or the user touches outside of the keyboard
+		self.hideKeyboard()
+		self.emailField.delegate = self
+		self.passwordField.delegate = self
 	
 	}
 
@@ -46,5 +53,72 @@ class SignInVC: UIViewController {
 			}
 		}
 	}
+	
+	@IBAction func signInTapped(_ sender: Any) {
+		if let email = emailField.text, let password = passwordField.text {
+			Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+				if error == nil {
+					print("MATT: Email User authenticated with Firebase")
+				} else {
+					Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+						if error != nil {
+							print("Matt: Unable to authenticate with Firebase using email")
+						} else {
+							print("MATT: Successfully authenticated with Firebase")
+						}
+					})
+				}
+			})
+		}
+	}
+	
+	
+	
+	
 }
+
+extension UIViewController
+{
+	func hideKeyboard()
+	{
+		let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+			target: self,
+			action: #selector(UIViewController.dismissKeyboard))
+		
+		view.addGestureRecognizer(tap)
+	}
+	
+	func dismissKeyboard()
+	{
+		view.endEditing(true)
+	}
+	
+	// To dismiss keyboard when return is pressed
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		self.view.endEditing(true)
+		return false
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
